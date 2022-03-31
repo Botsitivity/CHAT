c6eac99e582ec477ff8cb26041a54a7a79b27c05
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-03-31 14:13:10 -0400

    A

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
             <input type="text" id="text" placeholder="Send Bopy a Message!">
         </div>
 <button class="send"></button>
+
 <div class="circle1"></div>
 <div class="circle2"></div>
 <div class="circle3"></div>
@@ -22,21 +23,21 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Staatliches&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Staatliches&display=swap');
 .top{
-  font-family: 'Staatliches', cursive;
-  margin-top: 3rem;
+font-family: 'Staatliches', cursive;
+  margin-top: 7rem;
 font-style: normal;
 font-weight: 400;
 font-size: 64px;
 line-height: 80px;
 text-align: center;
 }
-.send{
-  color: white;
+.send{ 
+color: white;
 border-radius: 40rem;
 position: absolute;
 width: 61px;
 height: 61px;
-left: 1326px;
+left: 92%;
 bottom: 2rem;
 background: #FF653F;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -45,7 +46,7 @@ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 #form-input{
   z-index: 1;
   position: absolute;
-width: 1231px;
+width: 85%;
 height: 61px;
 left: 68px;
 bottom: 2rem;
@@ -61,17 +62,20 @@ font-style: normal;
 font-weight: 400;
 font-weight: bolder;
 font-size: 24px;
-width: 1100px;
+width: 85%;
 margin-left: 2rem;
 margin-top: .8rem;
   vertical-align: middle;
 }
+#text:active{
+  border: none;
+}
 .circle1{
 position: absolute;
-width: 76.23px;
-height: 83.37px;
-left: -348.26px;
-top: 450.35px;
+width: 20rem;
+height: 20rem;
+left: 15rem;
+top: 19rem;
 background: rgba(255, 163, 132, 0.75);
 transform: rotate(0.57deg);
 border-radius: 40rem;
