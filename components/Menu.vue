bd2eaaecccacd0cf4729bd5e66c8d8111ec89745
Anthony Zhen <anthony20151128@gmail.com>
2022-04-06 15:25:06 -0400

    fuck u react

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,12 +1,192 @@
 <template>
-<div></div>
+  <div class="outerMenu">
+    <link rel="stylesheet" href="https://use.typekit.net/bre5ozt.css">
+    <div class="innerMenu">
+        <ul class="menuSections">
+            <li><NuxtLink to="/" class="menuText">ABOUT</NuxtLink></li>
+            <li><NuxtLink to="/" class="menuText underline">CONTACT US</NuxtLink></li>
+        </ul>
+        <ul class="middleSection">
+            <li><NuxtLink to="/" class="menuText specialText">BOTSITIVITY</NuxtLink></li>
+        </ul>
+        <ul class="menuSections">
+            <li><NuxtLink to="/" class="menuText">SIGNUP</NuxtLink></li>
+            <li><NuxtLink to="/" class="menuText">LOGIN</NuxtLink></li>
+        </ul>
+        <ul class="menuSections" id="hamburgerOuter">
+            <div class="hamburger" id="hamburger">
+                <div class="hamburgerLine"></div>
+            </div>
+        </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'NuxtMenu'
-}
+  name: "NuxtMenu",
+};
 </script>
 <style scoped>
+ @import url("https://p.typekit.net/p.css?s=1&k=bre5ozt&ht=tk&f=34872.34873.34871&a=76201545&app=typekit&e=css");
 
+.outerMenu{
+    margin: 0;
+    z-index: 10;
+    position: relative;
+}
+
+.innerMenu{
+    display: flex;
+    justify-content: space-around;
+    background-color: #EEEEEE;
+    padding: 16px 0 16px 0;
+}
+
+.menuSections{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+.middleSection{
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+.middleSection li{
+    padding: 16px;
+}
+
+.menuSections li{
+    transition: 1s all ease;
+    border-top: 3px solid #ffb63f;
+    border-bottom: 3px solid #ffb63f;
+    float: left;
+    padding: 16px;
+}
+
+.menuSections li:hover{
+    transition: 1s all ease;
+    border-top: 3px solid #ff653f;
+    border-bottom: 3px solid #ff653f;
+}
+
+.menuText{
+    font-size: 0.9rem;
+    text-decoration: none;
+    color: black;
+}
+
+.specialText{
+    font-family: Arbotek,sans-serif;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 28px;
+}
+
+.hamburger{
+    position: relative;
+    top: -15%;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: all .5 ease;
+    text-align:justify;
+    z-index: 111;
+}
+
+.hamburgerLine{
+    height: 6px;
+    width: 45px;
+    background-color: black;
+    border-radius: 5px;
+    overflow: hidden;
+    transition: all 1s ease;
+}
+
+.hamburgerLine::after,
+.hamburgerLine::before{
+    content: "";
+    position: absolute;
+    width: 45px;
+    height: 6px;
+    border-radius: 5px;
+    background-color: black;
+    transition: .5 all ease;
+}
+
+.hamburgerLine::after{
+    transform: translateY(16px);
+    transition: 0.5s all ease;
+
+}
+
+.hamburgerLine::before{
+    transform: translateY(-16px);
+    transition: 0.5s all ease;
+
+}
+
+.hamburger.openMenu .hamburgerLine{
+    transition: 1s all ease;
+}
+
+
+.hamburger.openMenu .hamburgerLine::before{
+    transition: 0.5s all ease;
+    width: 30px;
+    transform: rotate(45deg) translate(6px,9px);
+}
+
+.hamburger.openMenu .hamburgerLine::after{
+    transition: 0.5s all ease;
+    width: 30px;
+    transform: rotate(-45deg) translate(6px,-9px);
+}
+
+#hamburgerOuter{
+    display: none;
+}
+
+.hamburgerMenu{
+    position:fixed;
+    padding:0;
+    margin:0;
+
+    top:0;
+    left:100%;
+
+    width: 100%;
+    height: 100%;
+    background:white;
+
+    z-index: 100;
+
+    transition: 0.5s all ease;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    text-align: left;
+}
+
+.hamburgerMenu.openHamburger {
+    transform: translateX(-100%);
+}
+
+
+.hamburgerSections li{
+    font-size: 5vw;
+    font-family: "ubuntu";
+    padding: 13px;
+}
 </style>
