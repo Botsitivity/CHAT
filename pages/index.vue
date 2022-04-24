dba68d688445dadf04e26668fa7731197f52f903
ewright516 <ewright51606@gmail.com>
2022-04-24 17:40:06 -0400

    annoying outline fix

--- pages/index.vue
+++ pages/index.vue
@@ -163,8 +163,8 @@ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   margin-top: .8rem;
   vertical-align: middle;
 }
-#text:active {
-  border: none;
+#text:focus {
+  outline: none
 }
 .circle1 {
   position: absolute;
